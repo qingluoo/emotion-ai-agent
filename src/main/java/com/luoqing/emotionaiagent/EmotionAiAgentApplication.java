6a4afebe896397fa3b5f4c359a073329c044c64c
qingluoo <113030300+qingluoo@users.noreply.github.com>
2025-12-25 16:32:21 +0800

    初始化项目

---
+++ src/main/java/com/luoqing/emotionaiagent/EmotionAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.luoqing.emotionaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmotionAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmotionAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luoqing/emotionaiagent/EmotionAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luoqing.emotionaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmotionAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
